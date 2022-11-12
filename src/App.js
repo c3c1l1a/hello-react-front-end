9ed9cb9fec65e8fe782ea13d087a9dba2d472d06
c3c1l1a <cesswangumukima@gmail.com>
2022-11-12 18:50:36 +0800

    Add greeting controller and fetch data and use redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
 import React from 'react';
 import { Provider } from 'react-redux';
-import RootController from "./controllers/root";
+import Greeting from "./components/greeting";
 import store from './redux/configureStore';
 
 import {
@@ -17,7 +14,7 @@ function App() {
     <Provider store={store}>
       <Router>
         <Routes>
-          <Route exact path="/" element={<RootController />}/>
+          <Route exact path="/" element={<Greeting />}/>
         </Routes>
       </Router>
     </Provider>
